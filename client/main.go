d90bae4bc956eb270b73a93cd23720a694bce648
MoKanove <mpjy000@gmail.com>
2026-07-06 13:17:47 +0800

    We did it!

---
+++ client/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/things-go/go-socks5"
+	"github.com/pelletier/go-toml/v2"
+)
+
+type Config struct {
+	Socks5 struct {
+		Listen string `toml:"listen"`
+		Port   int    `toml:"port"`
+	} `toml:"socks5"`
+}
+
+type LoggingRule struct{
+
+}
+
+func main() {
+	configPath := flag.String("c", "", "Config file path")
+	flag.Parse()
+
+	if *configPath == "" {
+		fmt.Println("Not have any parameter, minimal using ./ixa -c config_file_path")
+		os.Exit(1)
+	}
+
+	fileData, err := os.ReadFile(*configPath)
+	if err != nil {
+		fmt.Printf("Cannot read config file %v\n", err)
+		os.Exit(1)
+	}
+
+	var cfg Config
+	if err := toml.Unmarshal(fileData, &cfg); err != nil {
+		fmt.Printf("Cannot decode config file %v\n", err)
+		os.Exit(1)
+	}
+
+	addr := net.JoinHostPort(cfg.Socks5.Listen, fmt.Sprintf("%d", cfg.Socks5.Port))
+	server := socks5.NewServer()
+
+	fmt.Printf("SOCKS5 server listening on %s\n", addr)
+	if err := server.ListenAndServe("tcp", addr); err != nil {
+		fmt.Printf("Server failed: %v\n", err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module ixa-go
 
 go 1.26.4
 
-require github.com/pelletier/go-toml/v2 v2.4.3 // indirect
+require (
+	github.com/pelletier/go-toml/v2 v2.4.3 // indirect
+	github.com/things-go/go-socks5 v0.1.1 // indirect
+)
